f86a62f445005ade0251969fe02e99bf48056a25
Joaquin Morales <joaquin.moralestr@gmail.com>
2023-09-04 15:37:23 -0300

    Change turn each time the player do their move

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 const gameboard = document.querySelector('#gameboard')
 
+const Player = (name, mark) => {
+  const getName = () => name
+  const getMarker = () => mark
+  let isPlaying = false
+  const play = () => {
+    isPlaying = true
+    return isPlaying
+  }
+
+  return { getName, getMarker, play }
+}
+
+const player1 = Player('Steve', 'X')
+const player2 = Player('David', 'O')
+let turn = player1
+
 const Gameboard = (() => {
   let fields = [
-    ['x', 'o', 'x'],
-    ['o', 'x', 'o'],
-    ['x', 'o', 'x']
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
   ]
   const getFields = () => fields
 
   return { getFields }
+})()
+
+const Game = (() => {
+  const changeTurn = () => turn === player1 ? turn = player2 : turn = player1
+
+  return { changeTurn }
 })()
 
 const DisplayController = (() => {
@@ -16,19 +38,12 @@ const DisplayController = (() => {
     line.forEach(field => {
       const div = document.createElement('div')
       div.textContent = field
+      div.addEventListener('click', () => {
+        div.textContent = turn.getMarker()
+        Game.changeTurn()
+      })
       gameboard.appendChild(div)
     })
   });
 })()
 
-const Player = (name, mark) => {
-  const getName = () => name
-  const getMark = () => mark
-
-  return { getName, getMark }
-}
-
-const player1 = Player('Steve', 'x')
-const player2 = Player('Jhon', 'o')
-
-DisplayController
